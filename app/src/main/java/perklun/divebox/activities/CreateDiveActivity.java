2010eeb323c89089536d7535d379c972819ca5e5
Perk <perklun@gmail.com>
2016-04-12 22:35:08 -0700

    feat: Added new activities and fragments
    Added 3 new activities and 3 new fragments

---
+++ app/src/main/java/perklun/divebox/activities/CreateDiveActivity.java
@@ -0,0 +1,15 @@
+package perklun.divebox.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import perklun.divebox.R;
+
+public class CreateDiveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_dive);
+    }
+}
---
+++ app/src/main/java/perklun/divebox/activities/ViewDiveActivity.java
@@ -0,0 +1,15 @@
+package perklun.divebox.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import perklun.divebox.R;
+
+public class ViewDiveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_dive);
+    }
+}
